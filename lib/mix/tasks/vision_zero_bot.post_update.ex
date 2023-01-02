c8fbb4a00c0ca62c72c26d896ca6df9e755bff71
Mitchell Henke <mitchell@mitchellhenke.com>
2023-01-02 15:21:43 -0600

    yearly post hopefully

--- lib/mix/tasks/vision_zero_bot.post_update.ex
+++ lib/mix/tasks/vision_zero_bot.post_update.ex
@@ -23,72 +23,76 @@ defmodule Mix.Tasks.VisionZeroBot.PostUpdate do
     Finch.start_link(name: MyFinch)
     today = Date.utc_today()
 
+    cond do
+      System.get_env("WEEKLY") == "TRUE" ->
+        crashes = get_crashes(today.year)
+
+        yearly_total_fatalities =
+          Enum.filter(crashes, fn crash ->
+            get_in(crash, ["properties", "totfatl"]) > 0
+          end)
+          |> Enum.sum()
+
+        yearly_total_injuries =
+          Enum.filter(crashes, fn crash ->
+            get_in(crash, ["properties", "totinj"])
+          end)
+          |> Enum.sum()
+
+        tweet = """
+        Since the beginning of the year, traffic violence has killed #{yearly_total_fatalities} people and seriously injured #{yearly_total_injuries} people in our city.
+        #VisionZero #StopTrafficViolence
+        """
+
+        ExTwitter.update(tweet)
+
+      System.get_env("SCHEDULE") == "YEARLY" ->
+        last_year_crashes = get_crashes(today.year - 1)
+
+        yearly_total_fatalities =
+          Enum.filter(crashes, fn crash ->
+            get_in(crash, ["properties", "totfatl"]) > 0
+          end)
+          |> Enum.sum()
+
+        yearly_total_injuries =
+          Enum.filter(crashes, fn crash ->
+            get_in(crash, ["properties", "totinj"])
+          end)
+          |> Enum.sum()
+
+        tweet = """
+        In #{today.year - 1}, traffic violence killed #{yearly_total_fatalities} people and seriously injured #{yearly_total_injuries} people in our city.
+        #VisionZero #StopTrafficViolence
+        """
+    end
+  end
+
+  def get_crashes(year) do
     url =
-      "https://transportal.cee.wisc.edu/partners/community-maps/crash/public/crashesKML.do?filetype=json&startyear=#{today.year}&injsvr=K&injsvr=A&county=milwaukee"
+      "https://transportal.cee.wisc.edu/partners/community-maps/crash/public/crashesKML.do?filetype=json&startyear=#{year}&injsvr=K&injsvr=A&county=milwaukee"
 
-    crashes =
-      with req <- Finch.build(:get, url),
-           {:ok, %{body: body}} <- Finch.request(req, MyFinch),
-           {:ok, map} <- Jason.decode(body),
-           {:ok, features} <- Map.fetch(map, "features") do
-        features
-      end
-      |> Enum.map(fn feature ->
-        update_in(feature, ["properties", "date"], fn string_date ->
-          [month, day, year] = String.split(string_date, "/")
+    with req <- Finch.build(:get, url),
+         {:ok, %{body: body}} <- Finch.request(req, MyFinch),
+         {:ok, map} <- Jason.decode(body),
+         {:ok, features} <- Map.fetch(map, "features") do
+      features
+    end
+    |> Enum.map(fn feature ->
+      update_in(feature, ["properties", "date"], fn string_date ->
+        [month, day, year] = String.split(string_date, "/")
 
-          Date.new!(
-            String.to_integer(year),
-            String.to_integer(month),
-            String.to_integer(day)
-          )
-        end)
-        |> update_in(["properties", "totfatl"], &String.to_integer/1)
-        |> update_in(["properties", "totinj"], &String.to_integer/1)
+        Date.new!(
+          String.to_integer(year),
+          String.to_integer(month),
+          String.to_integer(day)
+        )
       end)
-      |> Enum.filter(fn feature ->
-        get_in(feature, ["properties", "muniname"]) == "MILWAUKEE"
-      end)
-
-    one_week_ago = Date.add(today, -7)
-
-    last_week =
-      Enum.filter(crashes, fn crash ->
-        Date.compare(get_in(crash, ["properties", "date"]), one_week_ago) in [:gt, :eq]
-      end)
-
-    last_week_total_fatalities =
-      Enum.map(last_week, fn crash ->
-        get_in(crash, ["properties", "totfatl"])
-      end)
-      |> Enum.sum()
-
-    last_week_total_injuries =
-      Enum.map(last_week, fn crash ->
-        get_in(crash, ["properties", "totinj"])
-      end)
-      |> Enum.sum()
-
-    yearly_total_fatalities =
-      Enum.map(crashes, fn crash ->
-        get_in(crash, ["properties", "totfatl"])
-      end)
-      |> Enum.sum()
-
-    yearly_total_injuries =
-      Enum.map(crashes, fn crash ->
-        get_in(crash, ["properties", "totinj"])
-      end)
-      |> Enum.sum()
-
-    last_week_formatted =
-      "#{Calendar.strftime(one_week_ago, "%m/%d")}-#{Calendar.strftime(today, "%m/%d")}"
-
-    tweet = """
-    Since the beginning of the year, traffic violence has killed #{yearly_total_fatalities} people and seriously injured #{yearly_total_injuries} people in our city.
-    #VisionZero #StopTrafficViolence
-    """
-
-    ExTwitter.update(tweet)
+      |> update_in(["properties", "totfatl"], &String.to_integer/1)
+      |> update_in(["properties", "totinj"], &String.to_integer/1)
+    end)
+    |> Enum.filter(fn feature ->
+      get_in(feature, ["properties", "muniname"]) == "MILWAUKEE"
+    end)
   end
 end
