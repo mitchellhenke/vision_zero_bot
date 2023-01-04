6794c6eb64d2c3b6d356ebcf2f9dca4d8a66ef69
Mitchell Henke <mitchell@mitchellhenke.com>
2023-01-03 19:33:36 -0600

    fix yearly and add mastodon

--- lib/mix/tasks/vision_zero_bot.post_update.ex
+++ lib/mix/tasks/vision_zero_bot.post_update.ex
@@ -24,17 +24,17 @@ defmodule Mix.Tasks.VisionZeroBot.PostUpdate do
     today = Date.utc_today()
 
     cond do
-      System.get_env("WEEKLY") == "TRUE" ->
+      System.get_env("SCHEDULE") == "WEEKLY" ->
         crashes = get_crashes(today.year)
 
         yearly_total_fatalities =
-          Enum.filter(crashes, fn crash ->
-            get_in(crash, ["properties", "totfatl"]) > 0
+          Enum.map(crashes, fn crash ->
+            get_in(crash, ["properties", "totfatl"])
           end)
           |> Enum.sum()
 
         yearly_total_injuries =
-          Enum.filter(crashes, fn crash ->
+          Enum.map(crashes, fn crash ->
             get_in(crash, ["properties", "totinj"])
           end)
           |> Enum.sum()
@@ -45,18 +45,19 @@ defmodule Mix.Tasks.VisionZeroBot.PostUpdate do
         """
 
         ExTwitter.update(tweet)
+        post_to_mastodon(tweet)
 
       System.get_env("SCHEDULE") == "YEARLY" ->
         last_year_crashes = get_crashes(today.year - 1)
 
         yearly_total_fatalities =
-          Enum.filter(crashes, fn crash ->
-            get_in(crash, ["properties", "totfatl"]) > 0
+          Enum.map(last_year_crashes, fn crash ->
+            get_in(crash, ["properties", "totfatl"])
           end)
           |> Enum.sum()
 
         yearly_total_injuries =
-          Enum.filter(crashes, fn crash ->
+          Enum.map(last_year_crashes, fn crash ->
             get_in(crash, ["properties", "totinj"])
           end)
           |> Enum.sum()
@@ -65,6 +66,9 @@ defmodule Mix.Tasks.VisionZeroBot.PostUpdate do
         In #{today.year - 1}, traffic violence killed #{yearly_total_fatalities} people and seriously injured #{yearly_total_injuries} people in our city.
         #VisionZero #StopTrafficViolence
         """
+
+        ExTwitter.update(tweet)
+        post_to_mastodon(tweet)
     end
   end
 
@@ -94,5 +98,28 @@ defmodule Mix.Tasks.VisionZeroBot.PostUpdate do
     |> Enum.filter(fn feature ->
       get_in(feature, ["properties", "muniname"]) == "MILWAUKEE"
     end)
+  end
+
+  def post_to_mastodon(content) do
+    access_token = System.fetch_env!("MASTODON_ACCESS_TOKEN")
+    headers = [{"authorization", "Bearer #{access_token}"}]
+
+    body =
+      %{
+        status: content
+      }
+      |> Jason.encode!()
+
+    headers = [{"content-type", "application/json"}, {"authorization", "Bearer #{access_token}"}]
+
+    response =
+      with req <- Finch.build(:post, "https://botsin.space/api/v1/statuses", headers, body),
+           {:ok, resp = %{status: status, headers: _headers, body: body}} <-
+             Finch.request(req, MyFinch),
+           true <- status == 200,
+           {:ok, json} <- Jason.decode(body),
+           {:ok, _id} <- Map.fetch(json, "id") do
+        resp
+      end
   end
 end
